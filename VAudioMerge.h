df28c029dcaf9ff81a88c2a0f45fafd297275726
杜成东 <cddu@iflytek.com>
2019-12-17 15:14:24 +0800

    初始化

---
+++ VAudioMerge.h
@@ -0,0 +1,16 @@
+//
+//  VAudioMerge.h
+//  Pods-VAudioMerge_Tests
+//
+//  Created by 杜成东 on 2019/12/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VAudioMerge : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ VAudioMerge.m
@@ -0,0 +1,12 @@
+//
+//  VAudioMerge.m
+//  Pods-VAudioMerge_Tests
+//
+//  Created by 杜成东 on 2019/12/17.
+//
+
+#import "VAudioMerge.h"
+
+@implementation VAudioMerge
+
+@end
